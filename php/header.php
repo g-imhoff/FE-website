e9a403e64fbc185c5a768d789f6d2c8ac8b39014
Guillaume IMHOFF <guillaume.imhoff170804@gmail.com>
2024-03-15 15:07:33 +0100

    start to design the header

---
+++ php/header.php
@@ -0,0 +1,11 @@
+<header> 
+    <a href="./index.php"><img src="./assets/img/logo.png" alt="logo" class="logo"></a>
+
+    <nav>
+        <ul>
+            <li><a href="./index.php">Home</a></li>
+            <li><a href="./about.php">About</a></li>
+            <li><a href="./contact.php">Contact</a></li>
+        </ul>
+    
+</header>

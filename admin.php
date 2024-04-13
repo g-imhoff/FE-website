f03dc4973b6d13e5fcdcb47c188f6c52b22be2dc
Guillaume IMHOFF <guillaume.imhoff170804@gmail.com>
2024-04-13 02:49:23 +0200

    fin de session de code

---
+++ admin.php
@@ -0,0 +1,26 @@
+<?php
+    require_once './lang/get-lang.php';
+    require_once './php-include/function/db-connect.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>FE</title>
+
+        <!-- CSS -->        
+        <link rel="stylesheet" href="./css/base.css">
+        <link rel="stylesheet" href="./css/font.css">
+
+        <!-- Favicon -->
+        <link rel="shortcut icon" href="./assets/ico/favicon.ico" type="image/x-icon">
+    </head>
+
+    <?php
+        include_once "./php-include/template/header.php";
+    ?>
+
+</html>

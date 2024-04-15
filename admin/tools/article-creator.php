250f7572dd8633e88cd401970457be803d060562
Guillaume IMHOFF <guillaume.imhoff170804@gmail.com>
2024-04-15 23:56:22 +0200

    save

--- admin/tools/article-creator.php
+++ admin/tools/article-creator.php
@@ -7,12 +7,22 @@
         <h1>Article Creator</h1>
 
         <form action="" method="post">
-            <label>Title</label>
+            <label>Title : </label>
             <input type="text" name="" id="">
-            <label>Markdown article :</label>
+
+            <label>Markdown article : </label>
             <textarea name="" id=""></textarea>
 
+            <label for="">Category : </label>
+            <input type="text" name="" id="">
 
+            <label for="">Made by : </label>
+            <input type="text" name="" id="">
+
+            <label for="">Date : </label>
+            <input type="date" name="" id="">
+
+            <button type="submit" name="submit"><p>Create</p></button>
         </form>
     </section>
 </body>

53c9a00cd8a812eab2524428dd107821001db152
Guillaume IMHOFF <guillaume.imhoff170804@gmail.com>
2024-04-21 01:02:01 +0200

    add more verification in php

--- db/users.php
+++ db/users.php
@@ -8,41 +8,35 @@ class Users {
     }
 
     public function createUser($email, $username, $password, $confirmPassword) {
-        $email = htmlspecialchars($email);
-        $username = htmlspecialchars($username);
-        $password = htmlspecialchars($password);
-        $confirmPassword = htmlspecialchars($confirmPassword);
-
-        $email = trim($email);
-        $username = trim($username);
-        $password = trim($password);
-        $confirmPassword = trim($confirmPassword);
+        $args = func_get_args();
+        $args = array_map('htmlspecialchars', $args);
+        $args = array_map('trim', $args);
 
         global $trad;
 
-        if (empty($email) || empty($username) || empty($password) || empty($confirmPassword)) {
+        if (empty($args[0]) || empty($args[1]) || empty($args[2]) || empty($args[3])) {
             return $trad["login"]["empty"];
         }
 
-        if (strlen($username) > 50) {
+        if (strlen($args[1]) > 50) {
             return $trad["login"]["userLong"];
         }
 
-        if (strlen($email) > 255) {
+        if (strlen($args[0]) > 255) {
             return $trad["login"]["emailLong"];
         }
 
-        if (strlen($password) > 128) {
+        if (strlen($args[2]) > 128) {
             return $trad["login"]["passLong"];
         }
 
         //doesnt need to check password confirmation because it needs anyways to be equal to password so < than 255 and not ""
 
-        if (!(filter_var($email, FILTER_VALIDATE_EMAIL))) {
+        if (!(filter_var($args[0], FILTER_VALIDATE_EMAIL))) {
             return $trad["login"]["emailInvalid"];
         }
 
-        if ($password !== $confirmPassword) {
+        if ($args[2] !== $args[3]) {
             return $trad["login"]["passNoMatch"];
         }
 
@@ -50,7 +44,7 @@ class Users {
 
         $queryVerifyEmail = $conn->prepare("SELECT id FROM `users` WHERE `email` = ?"); 
     
-        if ($queryVerifyEmail->execute([$email])) {
+        if ($queryVerifyEmail->execute([$args[0]])) {
             $resultVerifyEmail = $queryVerifyEmail->fetch();
         } else {
             return "Error: " . $sql . "<br>" . $conn->error;
@@ -62,7 +56,7 @@ class Users {
     
         $queryVerifyUsername = $conn->prepare("SELECT id FROM `users` WHERE `username` = ?"); 
     
-        if ($queryVerifyUsername->execute([$username])) {
+        if ($queryVerifyUsername->execute([$args[1]])) {
             $resultVerifyUsername = $queryVerifyUsername->fetch();
         } else {
             return "Error: " . $sql . "<br>" . $conn->error;
@@ -74,7 +68,7 @@ class Users {
     
         $queryCreate = $conn->prepare("INSERT INTO users (email, username, password) VALUES (?, ?, ?)");
     
-        if ($queryCreate->execute([$email, $username, $password])) {
+        if ($queryCreate->execute([$args[0], $args[1], $args[2]])) {
             return "Success";
         } else {
             return "Error: " . $sql . "<br>" . $conn->error;
@@ -82,21 +76,19 @@ class Users {
     }
 
     public function logUser($email, $password) {
-        $email = htmlspecialchars($email);
-        $password = htmlspecialchars($password);
+        $args = func_get_args();
+        $args = array_map('htmlspecialchars', $args);
+        $args = array_map('trim', $args);
 
-        $email = trim($email);
-        $password = trim($password);
-
-        if (empty($email) || empty($password)) {
+        if (empty($args[0]) || empty($args[1])) {
             return $trad["login"]["empty"];
         }
 
-        if (strlen($email) > 255) {
+        if (strlen($args[0]) > 255) {
             return $trad["login"]["emailLong"];
         }
 
-        if (strlen($password) > 128) {
+        if (strlen($args[1]) > 128) {
             return $trad["login"]["passLong"];
         }
 
@@ -104,14 +96,14 @@ class Users {
         
         $query = $conn->prepare("SELECT `email`, `password`, `username` FROM users WHERE `email` = ? OR `username` = ?");
         
-        if ($query->execute([$email, $email])) {
+        if ($query->execute([$args[0], $args[0]])) {
             $result = $query->fetch(PDO::FETCH_ASSOC);
         } else {
             return "Error: " . $sql . "<br>" . $conn->error;
         }
     
         if ($result !== NULL) {
-            if ($result['password'] == $password) {
+            if ($result['password'] == $args[1]) {
                 setcookie('email', $result['email'], [
                     'expires' => time() + 3600 * 24 * 7, // Exemple d'expiration dans une heure
                     'path' => '/', // Chemin pour lequel le cookie est valide
